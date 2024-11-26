1a9369eb0ad5044f58295cf88ac31ae6c734945e
Syedsamid <samidsyed1720@gmail.com>
2024-11-26 05:03:32 +0000

    added number and name

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const readline = require("readline-sync");
 
 let age = readline.questionInt("Enter your age? : ");
 console.log(`My age is ${age}`);
+
+let phone = readline.questionInt("Enter your phone number? : ");
+console.log(`What is your phone number is ${phone}`);
+
+let email = readline.questionEMail("Enter your email ");
+console.log(email);
