d4f8b871b818f17b2f49c61a1dbafec801d63a42
Syedsamid <samidsyed1720@gmail.com>
2024-11-27 01:54:10 +0000

    node introduction

---
+++ 1.js
@@ -0,0 +1,46 @@
+// Node JS : Its an Open Source JavaScript Runtime Environment
+// Which allowed JavaScript to run outside of Browser
+
+// How to Download Nodejs in Ubuntu :
+// 1. Visit nodejs.org
+// 2. Copy the download commands and paste it in your
+// local terminal
+
+/*
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+ // Then close the Terminal and Reopen the Terminal
+
+# download and install Node.js (you may need to restart the terminal)
+
+nvm install 22
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v22.11.0`
+
+# verifies the right npm version is in the environment
+npm -v # should print `10.9.0`
+
+*/
+
+
+/*
+// Steps to setup an Nodejs Application
+
+1. Create a folderName in camelCase of your nodejs 
+backend project
+2. cd folderName (make sure u check node -v and npm -v)
+3. npm init  or npm init -y  
+4  Install packages which you use using npm install or npm i example (npm install readline-sync or npm i readline-sync)
+5. creating .gitignore and adding node modules
+6. setting up tests in package.json
+7. setting up es6 type:"module"
+8. setting up config and .env
+9. using nodemon to run the nodejs app
+
+
+
+
+
+*/
